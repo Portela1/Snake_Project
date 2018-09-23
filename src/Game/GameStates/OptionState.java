3e92654907a29e003f5db9440fc68b30dac51c61
manuelportela <manuelportela@192.168.0.11>
2018-09-23 14:39:55 -0400

    Added 3 dummy options to change latter

--- src/Game/GameStates/OptionState.java
+++ src/Game/GameStates/OptionState.java
@@ -22,7 +22,24 @@ public class OptionState extends State {
         handler.getMouseManager().setUimanager(uiManager);
 
 
-        uiManager.addObjects(new UIImageButton(handler.getWidth()/2-64, handler.getHeight()/2-32, 10, 200, Images.butstart, new ClickListlener() {
+        uiManager.addObjects(new UIImageButton(handler.getWidth()/2-100, 400, 60, 30, Images.Options, () -> {
+            handler.getMouseManager().setUimanager(null);
+            State.setState(handler.getGame().optionState);
+        }));
+        
+        uiManager.addObjects(new UIImageButton(handler.getWidth()/2-100, 450, 60, 30, Images.Options, () -> {
+            handler.getMouseManager().setUimanager(null);
+            State.setState(handler.getGame().optionState);
+        }));
+        
+        uiManager.addObjects(new UIImageButton(handler.getWidth()/2-100, 500, 60, 30, Images.Options, () -> {
+            handler.getMouseManager().setUimanager(null);
+            State.setState(handler.getGame().optionState);
+        }));
+        
+        
+        
+        uiManager.addObjects(new UIImageButton(handler.getWidth()/2-55, handler.getHeight()/2-32, 128, 64, Images.butstart, new ClickListlener() {
             @Override
             public void onClick() {
                 handler.getMouseManager().setUimanager(null);
